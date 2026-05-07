fcead6b956e8ae2006152f696e746be50812ba16
angeldavid <angeldavidhernandez23@gmail.com>
2026-05-06 23:27:03 -0600

    Frontend integrado con Render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 // 👇 IMPORTAR TODAS LAS RUTAS
 const clientesRoutes = require("./routes/clientes");
@@ -13,20 +14,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// 👇 MOSTRAR CARPETA PUBLIC
+app.use(express.static(path.join(__dirname, "public")));
+
 // 🔗 Conexión a MongoDB Atlas
-mongoose.connect("mongodb+srv://angeldavidhernandez23_db_user:david980@cluster0.rvynwvf.mongodb.net/tienda?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Conectado a MongoDB Atlas ✅"))
   .catch(err => console.log("Error de conexión ❌", err));
 
-// Ruta de prueba
-app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
-});
-
-// 👇 USAR TODAS LAS RUTAS
+// 👇 RUTAS API
 app.use("/clientes", clientesRoutes);
 app.use("/productos", productosRoutes);
 app.use("/ventas", ventasRoutes);
+
+// 👇 ABRIR INDEX.HTML
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Levantar servidor
 app.listen(3000, () => {
